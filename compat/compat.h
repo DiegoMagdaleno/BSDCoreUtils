5e91296be123e23d3ac11d3da926bb6109eac9d5
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-02-17 20:02:35 -0600

    Redefine SIMPLEQ_NEXT

--- compat/compat.h
+++ compat/compat.h
@@ -111,21 +111,23 @@ struct mtget
 #undef SIMPLEQ_END
 #undef SIMPLEQ_NEXT
 
-#define SIMPLEQ_FIRST(head) ((head)->sqh_first)
-#define SIMPLEQ_EMPTY(head) (SIMPLEQ_FIRST(head) == SIMPLEQ_END(head))
-#define SIMPLEQ_END(head) NULL
 
-#define SIMPLEQ_HEAD(name, type) \
-    struct name                  \
-    {                            \
-        struct type *sqh_first;  \
-        struct type **sqh_last;  \
+#define	SIMPLEQ_FIRST(head)	    ((head)->sqh_first)
+#define	SIMPLEQ_EMPTY(head)	    (SIMPLEQ_FIRST(head) == SIMPLEQ_END(head))
+#define	SIMPLEQ_END(head)	    NULL
+#define	SIMPLEQ_NEXT(elm, field)    ((elm)->field.sqe_next) 
+
+#define SIMPLEQ_HEAD(name, type)                                \
+    struct name                                                 \
+    {                                                           \
+        struct type *sqh_first; /* first element */             \
+        struct type **sqh_last; /* addr of last next element */ \
     }
 
-#define SIMPLEQ_ENTRY(type)    \
-    struct                     \
-    {                          \
-        struct type *sqe_next; \
+#define SIMPLEQ_ENTRY(type)                       \
+    struct                                        \
+    {                                             \
+        struct type *sqe_next; /* next element */ \
     }
 
 #define SIMPLEQ_INSERT_TAIL(head, elm, field)      \
@@ -136,10 +138,8 @@ struct mtget
         (head)->sqh_last = &(elm)->field.sqe_next; \
     } while (0)
 
-#define SIMPLEQ_HEAD_INITIALIZER(head) \
-    {                                  \
-        NULL, &(head).sqh_first        \
-    }
+#define SIMPLEQ_HEAD_INITIALIZER(head)					\
+	{ NULL, &(head).sqh_first }
 
 #define SIMPLEQ_FOREACH(var, head, field) \
     for ((var) = SIMPLEQ_FIRST(head);     \
