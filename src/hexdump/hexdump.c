83ef77243b21d8ee7cb02c4e85860b93a6621352
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-08-06 19:38:27 -0500

    Clean unecessary stuff in hexdump, renable building hexdump on Linux

--- src/hexdump/hexdump.c
+++ src/hexdump/hexdump.c
@@ -29,21 +29,10 @@
  * SUCH DAMAGE.
  */
 
-#ifndef lint
-static const char copyright[] =
-"@(#) Copyright (c) 1989, 1993\n\
-	The Regents of the University of California.  All rights reserved.\n";
-#endif /* not lint */
 
-#ifndef lint
-#if 0
-static char sccsid[] = "@(#)hexdump.c	8.1 (Berkeley) 6/6/93";
-#endif
-#endif /* not lint */
+
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
 
-#include <sys/types.h>
 
 #include <err.h>
 #include <locale.h>
