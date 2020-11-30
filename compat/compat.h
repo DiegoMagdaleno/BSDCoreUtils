4c3c7198a21a2c42b8301e01e3f44db9507a7bff
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-11-29 19:37:35 -0600

    There is no need for logtwp anymore

--- compat/compat.h
+++ compat/compat.h
@@ -55,12 +55,6 @@ const char *group_from_gid(gid_t, int);
 int uid_from_user(const char *, uid_t *);
 int gid_from_group(const char *, gid_t *);
 
-/* logwtmp.c */
-void logwtmp(const char *, const char *, const char *);
-
-/* Darwin's version of logwtmp.c*/
-void darwinlogwtmpx(const char*, const char*, const char*);
-
 /* fmt_scaled.c */
 int scan_scaled(char *, long long *);
 int fmt_scaled(long long, char *);
