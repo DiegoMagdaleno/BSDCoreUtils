64a991c21be8a7e9706e524c9b846c6d64fc8e27
Diego Magdaleno <diegomagdaleno@protonmail.com>
2021-08-11 11:08:25 -0500

    [POSIX] Start sorting portability warning with PRId64 migrations

--- src/du/du.c
+++ src/du/du.c
@@ -46,6 +46,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <util.h>
+#include <inttypes.h>
 
 #include <sys/param.h>
 #include "compat.h"
@@ -304,14 +305,14 @@ void
 prtout(int64_t size, char *path, int hflag)
 {
 	if (!hflag)
-		(void)printf("%lld\t%s\n", size, path);
+		(void)printf("%" PRId64 "\t%s\n", size, path);
 	else {
 		char buf[FMT_SCALED_STRSIZE];
 
 		if (fmt_scaled(size * 512, buf) == 0)
 			(void)printf("%s\t%s\n", buf, path);
 		else
-			(void)printf("%lld\t%s\n", size, path);
+			(void)printf("%" PRId64 "\t%s\n", size, path);
 	}
 }
 
