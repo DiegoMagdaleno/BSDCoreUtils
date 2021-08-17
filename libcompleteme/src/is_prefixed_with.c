6a941f69222fbb99587227b1ed4220673fcd57e1
Diego Magdaleno <diegomagdaleno@protonmail.com>
2021-08-17 14:22:34 -0500

    [libcompleteme] Add is_prefixed_with

---
+++ libcompleteme/src/is_prefixed_with.c
@@ -0,0 +1,23 @@
+#include "is_prefixed_with.h"
+#include "first_component.h"
+
+bool
+is_prefixed_with (const char *path, const char *prefix)
+{
+  struct list *comp;
+  struct lnode *nd;
+
+  if (!path)
+    {
+      return NULL;
+    }
+
+  comp = get_comp_of_path (path);
+  nd = getatindx (comp, 0);
+  if (!nd)
+    {
+      return NULL;
+    }
+
+  return (bool)strcmp (prefix, nd->component);
+}
---
+++ libcompleteme/src/is_prefixed_with.h
@@ -0,0 +1,14 @@
+
+#include "is"
+bool is_prefixed_with(const char *filename) {
+    if (!filename) {
+        return NULL;
+    }
+
+    if 
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool is_prefixed_with(const char*);
