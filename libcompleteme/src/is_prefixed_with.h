db1da0bb557c93c7332018d889681731c1453d5f
Diego Magdaleno <diegomagdaleno@protonmail.com>
2021-08-17 14:23:34 -0500

    [libcompleteme] Fix wrong declaration in header

--- libcompleteme/src/is_prefixed_with.h
+++ libcompleteme/src/is_prefixed_with.h
@@ -1,14 +1,5 @@
-
-#include "is"
-bool is_prefixed_with(const char *filename) {
-    if (!filename) {
-        return NULL;
-    }
-
-    if 
-}
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool is_prefixed_with(const char*);
+bool is_prefixed_with(const char*, const char*);
